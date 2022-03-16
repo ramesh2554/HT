43ad116d9686eb195c08c13d66e38c80bc83ad1b
Konakalla <895207@cognizant.com>
2022-03-16 11:57:48 +0530

    delete_mapping_added

--- OrderManagementSystem/src/main/java/com/oms/controller/OmsController.java
+++ OrderManagementSystem/src/main/java/com/oms/controller/OmsController.java
@@ -49,5 +49,10 @@ public class OmsController {
 	}
 	
 	
-
+	@DeleteMapping("deleteOrder/{id}")
+	public String deleteByid(@PathVariable Integer id) {
+		
+		service.deleteByid(id);
+		return "Deleted  id : "+id;
+	}
 }

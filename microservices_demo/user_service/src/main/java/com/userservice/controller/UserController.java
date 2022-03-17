f4753db12c9f9986863fbbf89ade95907b54b83a
Konakalla <895207@cognizant.com>
2022-03-17 17:08:21 +0530

    update chnages

---
+++ microservices_demo/user_service/src/main/java/com/userservice/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.userservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.userservice.model.User;
+import com.userservice.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping("/{userId}")
+	public User getUser(@PathVariable("userId") Long userId) {
+		
+		return service.getUser(userId);
+	}
+}
